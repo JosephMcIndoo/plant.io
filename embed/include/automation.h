681f1774d9472713cc4115f609f0a0e6dac51374
William Walker <24390822+superstealthysheep@users.noreply.github.com>
2024-03-30 21:27:03 -0500

    end makeathon

--- embed/include/automation.h
+++ embed/include/automation.h
@@ -1,7 +1,8 @@
 #ifndef AUTOMATION_H
 #define AUTOMATION_H
+#define value u32_t
 
-static int Stack[32];
+static value Stack[32];
 static int SP = 0;
 #define PUSH(val) Stack[SP++]=val // TODO: ensure index safety
 #define POP() Stack[--SP]
@@ -11,13 +12,47 @@ static int SP = 0;
 static void (*actions[FP_COUNT])(void); // function pointers hadsoimoismdcoisdjfaosmcoisdf
 static int (*sensors[FP_COUNT])(void);
 
+// implement n-arity later
+// #define MAX_ARGS 4;
+// static u32_t ArgBuf[4];
+// static u32_t RetVal;
+
+// static void
+
 void no_op() {}
 int zero() {return 0;}
 void blink(int times);
 void blink5();
 
+typedef struct {
+    void* fn;
+    u32_t arity;
+    // u32_t returns; // bool
+} fn_meta;
+
 void function_pointer_init();
-void interpret(char bytecode[], int bc_length);
+
+// given a stream of bytes, attempt to interpret and execute
+void interpret(char* bytecode, int bc_length);
 int read_15();
+
+enum Opcode {
+    OP_NONE =  '\0',
+    OP_PUSH, // read next 4 bytes from input, push to stack
+    OP_POP,
+    OP_EXEC, // always push the return value. user can throw it away
+    OP_EXECIF,
+    OP_GETFP, // look up function pointer given index
+    OP_LT,
+    OP_EQ,
+    OP_GT,
+    OP_LEQ,
+    OP_NEQ,
+    OP_GEQ,
+    OP_ADD,
+    OP_SUB,
+    OP_MULT,
+    OP_DIV,
+};
 
 #endif
--- embed/src/automation/automation.c
+++ embed/src/automation/automation.c
@@ -36,6 +36,7 @@ int read_15() {
 }
 
 
+
 // i: if
 // s: sensor
 // a: action
@@ -47,49 +48,111 @@ int read_15() {
 // () parens
 //  : null/empty/ignore
 
-// VERY SCUFFED
-void interpret(char bytecode[], int bc_len) {
-    SP = 0;
-    // "i c= s1 v1 a1" // prefix-order
-    // bytecode = "a0 s0 v1 c= i "; // stack-based/reverse polish notation
-    for (int i=0; i<bc_len; ++i) {
-        switch (bytecode[i]) {
-            case 'a': // push action to stack
-               int action_num = bytecode[++i]-'0'; // TODO: decode this better in the future
-               PUSH(actions[action_num]); // note: function pointer is also 32-bit, so we can push to stack
+// // VERY SCUFFED
+// void interpret(char bytecode[], int bc_len) {
+//     SP = 0;
+//     // "i c= s1 v1 a1" // prefix-order
+//     // bytecode = "a0 s0 v1 c= i "; // stack-based/reverse polish notation
+//     for (int i=0; i<bc_len; ++i) {
+//         switch (bytecode[i]) {
+//             case 'a': // push action to stack
+//                int action_num = bytecode[++i]-'0'; // TODO: decode this better in the future
+//                PUSH(actions[action_num]); // note: function pointer is also 32-bit, so we can push to stack
 
-            break;
-            case 's': // push sensor reading to stack
-                int sensor_num = bytecode[++i]-'0';
-                PUSH(sensors[sensor_num]());
+//             break;
+//             case 's': // push sensor reading to stack
+//                 int sensor_num = bytecode[++i]-'0';
+//                 PUSH(sensors[sensor_num]());
 
-            break;
-            case 'v': // push value (0-9) to stack (TODO: extend)
-                int value = bytecode[++i]-'0';
-                PUSH(value);
+//             break;
+//             case 'v': // push value (0-9) to stack (TODO: extend)
+//                 int value = bytecode[++i]-'0';
+//                 PUSH(value);
 
-            break;
-            case 'c': // for time being, just assume comparison is ==
-                char comparator = bytecode[++i];
-                int rhs = POP();
-                int lhs = POP();
-                switch (comparator) {
-                    case '=':
-                        PUSH(lhs == rhs);
-                    break;
-                    default:
-                        PUSH(-1);
+//             break;
+//             case 'c': // for time being, just assume comparison is ==
+//                 char comparator = bytecode[++i];
+//                 int rhs = POP();
+//                 int lhs = POP();
+//                 switch (comparator) {
+//                     case '=':
+//                         PUSH(lhs == rhs);
+//                     break;
+//                     default:
+//                         PUSH(-1);
+//                 }
+
+//             break;
+//             case 'i': // if condition then response
+//                 int condition = POP();
+//                 void (*response)(void) = (void*) POP();
+//                 if (condition) {
+//                     response();
+//                 }
+
+//             break;
+//         }
+//     }
+// }
+
+void interpret(char* bytecode, int bc_length) {
+    char* curr = bytecode;
+    char* end = bytecode + bc_length*sizeof(char);
+    for (char* curr = bytecode; curr<end; curr++) {
+        switch (*curr) {
+            case OP_NONE: break;
+            case OP_PUSH:
+                // big endian: int32_t val = ((int32_t*) curr); // TODO: endianness?
+                // need to do it this way bc little endian
+                uint32_t val = 0;
+                for (int i=0; i<4; ++i) {
+                    val <<= 8;
+                    val += (uint32_t) *(++curr); // darn i hope this works
                 }
-
-            break;
-            case 'i': // if condition then response
-                int condition = POP();
-                void (*response)(void) = (void*) POP();
-                if (condition) {
-                    response();
+                PUSH(val);
+                break;
+            case OP_POP: POP(); break;
+            case OP_EXEC:
+                fn_meta* meta = (fn_meta*) POP();
+                uint32_t ret = 0;
+                if (meta->arity == 0) {
+                    value (*fun)(value, value, value) = meta->fn;
+                    ret = fun();
+                } else if (meta->arity == 1) {
+                    value (*fun)(value) = meta->fn; 
+                    ret = fun(POP());
+                } else if (meta->arity == 2) {
+                    value (*fun)(value, value) = meta->fn;
+                    ret = fun(POP(), POP());
+                } else if (meta->arity == 3) {
+                    value (*fun)(value, value, value) = meta->fn;
+                    ret = fun(POP(), POP(), POP());
+                } else if (meta->arity == 4) {
+                    value (*fun)(value, value, value, value) = meta->fn;
+                    ret = fun(POP(), POP(), POP(), POP());
+                } // TODO: error handle if arity out of range?
+                if (meta->returns) {
+                    push(ret);
                 }
+                //     // case 0: // find better way to do this
+                //     //     uint32_t (*fun)(void) = meta->fn; 
+                //     //     break;
+                //     case 1: // find better way to do this
+                //         uint32_t (*fun)(value) = meta->fn; 
+                //         ret = fun(POP());
+                //         break;
+                //     case 2:
+                //         value (*fun)(value, value) = meta->fn;
+                //         ret = fun(POP(), POP());
+                //         break;
 
-            break;
+                // }
+                // just assume 0-arity for now
+                // uint32_t (*fun)(void) = meta->fn;
+                // ret = fun();
+
+                PUSH(ret);
+                break;
         }
     }
 }
